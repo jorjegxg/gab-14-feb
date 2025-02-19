636ffda64f84810edfe23206ed0d110ac615f102
Gheorghe Luța <georgelutaoff@gmail.com>
2025-02-20 01:11:11 +0200

    mergi ?

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    output: 'export', // Permite exportul static
+    images: {
+      unoptimized: true, // Dacă folosești imagini
+    },
+  };
+  
