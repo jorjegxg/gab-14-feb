954698eba869a8d40d59e717e1de570f556e708f
Gheorghe Luța <georgelutaoff@gmail.com>
2025-02-20 01:43:17 +0200

    huh ?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Permite exportul static
+    "output": 'export', // Permite exportul static
   };
 
 module.exports = nextConfig;
