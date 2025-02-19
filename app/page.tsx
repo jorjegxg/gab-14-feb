a6a7c697560383476262eec8132f7a6ef48bdc99
Gheorghe Luța <georgelutaoff@gmail.com>
2025-02-20 01:16:42 +0200

    am revenit din test

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,85 @@
-// "use client";
-// import Calendar from "./components/Calendar";
-// import Cronometru from "./components/Cronometru";
-// import FancyText from "./components/FancyText";
-// import LazyShow from "./components/LazyShow";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <div className="bg-gradient-to-r to-pink-200 via-red-200 from-blue-200">
-
-
-
-//         <LazyShow>
-//           <Cronometru />
-//         </LazyShow>
-
-//         <LazyShow>
-//           <Calendar />
-//         </LazyShow>
-
-//         <div className="">
-
-//           <LazyShow>
-//             <div className='flex  justify-center'>
-//               <video
-//                 autoPlay
-//                 loop
-//                 muted
-//                 className="top-0 left-0 w-full h-[600px]  object-cover z-0"
-//               >
-//                 <source src="/Gab/video_gabi.mp4" type="video/mp4" />
-//                 Your browser does not support the video tag.
-//               </video>
-
-
-//             </div>
-//           </LazyShow>
-
-//         </div>
-
-
-
-
-
-
-
-//         <LazyShow>
-//           <div className="flex justify-center items-center ">
-
-//             ......
-//           </div>
-//         </LazyShow>
-
-
-
-
-
-
-
-//         <LazyShow>
-//           <div className="flex justify-center items-center ">
-
-//             <FancyText />
-//           </div>
-//         </LazyShow>
-//       </div>
-
-
-
-
-//     </>
-//   );
-// }
-
-// // Această funcție generează pagina static la build
-// export async function getStaticParams() {
-//   return {
-//     props: {
-//       message: "Acest text este generat static!",
-//     },
-//   };
-// }
-
-
-// app/page.js
 "use client";
+import Calendar from "./components/Calendar";
+import Cronometru from "./components/Cronometru";
+import FancyText from "./components/FancyText";
+import LazyShow from "./components/LazyShow";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Pagina Statică de Test</h1>
-      <p>Acesta este un exemplu simplu de pagină statică în Next.js.</p>
-      <p>
-        Dacă poți vedea acest text, înseamnă că totul este configurat corect!
-      </p>
-    </div>
+    <>
+      <div className="bg-gradient-to-r to-pink-200 via-red-200 from-blue-200">
+
+
+
+        <LazyShow>
+          <Cronometru />
+        </LazyShow>
+
+        <LazyShow>
+          <Calendar />
+        </LazyShow>
+
+        <div className="">
+
+          <LazyShow>
+            <div className='flex  justify-center'>
+              <video
+                autoPlay
+                loop
+                muted
+                className="top-0 left-0 w-full h-[600px]  object-cover z-0"
+              >
+                <source src="/Gab/video_gabi.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+
+
+            </div>
+          </LazyShow>
+
+        </div>
+
+
+
+
+
+
+
+        <LazyShow>
+          <div className="flex justify-center items-center ">
+
+            ......
+          </div>
+        </LazyShow>
+
+
+
+
+
+
+
+        <LazyShow>
+          <div className="flex justify-center items-center ">
+
+            <FancyText />
+          </div>
+        </LazyShow>
+      </div>
+
+
+
+
+    </>
   );
 }
+
+// Această funcție generează pagina static la build
+export async function getStaticParams() {
+  return {
+    props: {
+      message: "Acest text este generat static!",
+    },
+  };
+}
+
+
