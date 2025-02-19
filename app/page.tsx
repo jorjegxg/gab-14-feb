9f508b2705d6180e3c4df1b7b4ba7607a4adb624
Gheorghe Luța <georgelutaoff@gmail.com>
2025-02-20 01:30:42 +0200

    video te rog

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                 muted
                 className="top-0 left-0 w-full h-[600px]  object-cover z-0"
               >
-                <source src="/Gab/video_gabi.mp4" type="video/mp4" />
+                <source src="../public/GAB/video_gabi.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
 
